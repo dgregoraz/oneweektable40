d56496d4f6c735944b1e0eaab428cf97ef0a6782
Diego Gregoraz <36630002+dgregoraz@users.noreply.github.com>
2018-10-11 13:34:36 -0700

    new version. Ready to prod.

--- notebooks/Users/digregor@microsoft.com/preprocessing.scala
+++ notebooks/Users/digregor@microsoft.com/preprocessing.scala
@@ -1,10 +1,2 @@
 // Databricks notebook source
 print("this is version 1.1")
-
-
-// COMMAND ----------
-
-val veryNestedDF = Seq("Diego", "Daniel").toDF()
-
-//veryNestedDF.show()
-HelloWorld.withGreeting(veryNestedDF)
